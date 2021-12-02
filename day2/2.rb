45210f48ca6f7bd921213c5fb00c39e0ab8de2c7
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-02 19:25:54 +0200

    Day 2

--- day2/2.rb
+++ day2/2.rb
@@ -14,3 +14,23 @@ data.each do |dir, n|
 end 
 
 puts d_x * d_y
+
+
+# Part 2
+
+
+d_x = 0 
+d_y = 0 
+aim = 0
+data.each do |dir, n| 
+    case dir 
+    when 'forward' then d_x += n.to_i and d_y += n.to_i * aim
+    when 'up' then aim -= n.to_i 
+    when 'down' then aim += n.to_i 
+    else 
+        raise " I do not no where to go"
+    end   
+    
+end 
+
+puts d_x * d_y
