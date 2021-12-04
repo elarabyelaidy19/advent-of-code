313ab0b197200485bab9a4db6a6e1d969d7da5d4
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-04 12:42:21 +0200

    day 3

--- day3/3.rb
+++ day3/3.rb
@@ -15,7 +15,37 @@ end
 
 most_common = counts.map { |cnt| cnt >= data.length / 2 ? 1 : 0 }  
 least_common = most_common.map { |bit| bit == 0 ? 1 : 0 } 
-puts to_decimal(most_common) * to_decimal(least_common) 
+#puts to_decimal(most_common) * to_decimal(least_common) 
+
+
+# part two 
+
+def filter_by(data, value) 
+    remaining = data.sort   
+    ruler = value ? '1' : '0' 
+
+    data[0].length.times do |i| 
+        ones = remaining.count { |d| d[i] == '1' } 
+        zeros = remaining.count - ones 
+
+        if ones == zeros 
+            choose = ruler 
+        else 
+            if value 
+                choose = ones > zeros ? '1' : '0' 
+            else 
+                choose = ones > zeros ? '0' : '1' 
+            end 
+        end 
+    
+        remaining.select! { |d| d[i] == choose } 
+        return to_decimal(remaining[0]) if remaining.length == 1 
+    end 
+
+end 
+
+puts filter_by(data, true) * filter_by(data, false) 
+
 
 
 
