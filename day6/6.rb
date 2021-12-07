166414f027e56121ab1aa08f504c00b99ed609b5
elarabyelaidy19 <elarabyelaidy@gmail.com>
2021-12-07 18:01:50 +0200

    day 6 complete

--- day6/6.rb
+++ day6/6.rb
@@ -5,12 +5,10 @@ timers = Array.new(9, 0)
 
 fish_timer.each do |timer|  
     timers[timer] += 1  
-
 end 
 
 
 def simulate_step(timers) 
-
     zeroes = timers[0]
     timers.each_index do |i|
       next if i == 0
@@ -23,6 +21,9 @@ end
 
 
 80.times { simulate_step(timers) }
+puts timers.sum
+
+(256 - 80).times { simulate_step(timers) }
 puts timers.sum
 
 
